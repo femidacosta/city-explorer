936cba64b46f443228d791f4d5dd3b89fe46001b
Femi Da Costa <femi.dacosta@proton.me>
2023-03-06 14:35:10 +0000

    created city-explorer app.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  //query API
+  const [location, setlocation] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
+
+  console.log(process.env.REACT_APP_CITY_KEY);
+
+  //function to retrieve location data
+  async function getLocation() {
+    const API = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${searchQuery}&format=json`;
+    const res = await axios.get(API);
+    console.log(res.data[0]);
+    setlocation(res.data[0]);
+  }
+  function handleSearch(e) {
+    setSearchQuery(e.target.value);
+    //event is the input, target, the event being targetted. value is the data
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {location.display_name && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {location.display_name} Is at lat and lon: {location.lat} /
+          {location.lon}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      <input onChange={handleSearch} placeholder="Search for a city" />
+      <button onClick={getLocation}>Get Data</button>
     </div>
   );
 }
